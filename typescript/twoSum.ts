cf3729fd37aec9a4af0775b3285006e79adbbf31
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-17 08:15:43 +0000

    added typescript twoSum

---
+++ typescript/twoSum.ts
@@ -0,0 +1,20 @@
+function twoSum(nums: number[], target: number): number[] {
+    let j: number;
+    let i: number;
+    let result: number[] = [];
+    
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                result.push(i, j);
+                return result;
+            }
+        }
+    }
+    
+    return result;
+};
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
