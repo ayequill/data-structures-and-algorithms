c6e42d26414b87f8f50749cc2eaf4a6ab668e4a9
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-01 22:47:46 +0000

    Refactored

--- python/isPalindrome.py
+++ python/isPalindrome.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        num_str = str(x)
-        test_str  = num_str[len(num_str)-1::-1]
-        if test_str == num_str:
-            return True 
-        return False
+        # num_str = str(x)
+        # test_str  = num_str[len(num_str)-1::-1]
+        # if test_str == num_str:
+        #     return True 
+        # return False
+        return True if str(x) == str(x)[::-1] else False
 
 
 pali  = Solution()
 print(pali.isPalindrome(121))
 print(pali.isPalindrome(10))
+print(pali.isPalindrome(1221))
